91707f8c05e519c1cb30255321661daa887a83bd
AngryHaz <stardad88@gmail.com>
2019-10-22 22:23:41 +0500

    Commit 4

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -4,10 +4,18 @@ using namespace std;
 
 int main(){ 
 
-	cout << "Hello world" << endl;
-	int a;
+	cout << "Hello world!" << endl;
+	char a[100]; 
+	bool IsBreak=false; 	
+	cout << "Введите число" << endl; 
 	cin >> a;
-	cout << a << endl;  
- 
-return 0; 
+	for (int i=0; a[i]!='\0'; i++) 
+	if (a[i]<48 || a[i]>57) { 
+		cout << "Это не число!\n";  
+		break; 
+	} 
+
+	cout << a << endl; 
+
+	return 0; 
 }
